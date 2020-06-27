adda5b8b5af7a821cfafbf9ae8a5fb0c865c630f
yNot-SD <tonysduarte101@gmail.com>
2020-06-27 11:02:46 -0300

    Login JWT - Completo

--- LoginJWT/controllers/userController.js
+++ LoginJWT/controllers/userController.js
@@ -1,9 +1,13 @@
 const User = require("../models/User")
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken")
+const { loginValidate, registerValidate } = require('./validate')
 
 const userController = {
     register: async function (req, res) {
+
+        const { error } = registerValidate(req.body)
+        if (error) { return res.status(400).send(error.message) }
 
         const selectedUser = await User.findOne({ email: req.body.email })
         if (selectedUser) return res.status(400).send('Email alredy exists')
@@ -22,6 +26,10 @@ const userController = {
         }
     },
     login: async function (req, res) {
+
+        const { error } = loginValidate(req.body)
+        if (error) { return res.status(400).send(error.message) }
+
         const selectedUser = await User.findOne({ email: req.body.email })
         if (!selectedUser) return res.status(400).send('Email or password incorrect')
 
